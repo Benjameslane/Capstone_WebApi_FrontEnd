f1a595d05893acbb9293a7a84d071d2acc98b271
Benjameslane <ben.james.lane9>
2023-10-26 15:24:44 -0600

    fixed user registration

--- src/Services/FoodItemService.js
+++ src/Services/FoodItemService.js
@@ -1,7 +1,7 @@
 // services/foodItemService.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://localhost:5001/api';
 
 const addFoodItem = async (foodItem) => {
   try {
